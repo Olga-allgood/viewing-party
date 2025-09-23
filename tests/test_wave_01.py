69346969d31a1131c25b6e4b7c31447c92e36463
Olga-allgood <olga.s.orlova@gmail.com>
2025-09-23 13:09:41 -0700

    Waves 2 and 3 added for review

--- tests/test_wave_01.py
+++ tests/test_wave_01.py
@@ -32,7 +32,7 @@ def test_create_no_title_movie():
     # Assert
     assert new_movie is None
 
-@pytest.mark.skip()
+# @pytest.mark.skip()
 def test_create_no_genre_movie():
     # Arrange
     movie_title = "Title A"
@@ -45,7 +45,7 @@ def test_create_no_genre_movie():
     # Assert
     assert new_movie is None
 
-@pytest.mark.skip()
+# @pytest.mark.skip()
 def test_create_no_rating_movie():
     # Arrange
     movie_title = "Title A"
@@ -58,7 +58,7 @@ def test_create_no_rating_movie():
     # Assert
     assert new_movie is None
 
-@pytest.mark.skip()
+# @pytest.mark.skip()
 def test_adds_movie_to_user_watched():
     # Arrange
     movie = {
@@ -79,7 +79,7 @@ def test_adds_movie_to_user_watched():
     assert updated_data["watched"][0]["genre"] == GENRE_1
     assert updated_data["watched"][0]["rating"] == RATING_1
 
-@pytest.mark.skip()
+# @pytest.mark.skip()
 def test_adds_movie_to_non_empty_user_watched():
     # Arrange
     movie = {
@@ -99,7 +99,7 @@ def test_adds_movie_to_non_empty_user_watched():
     assert movie in updated_data["watched"]
     assert FANTASY_2 in updated_data["watched"]
 
-@pytest.mark.skip()
+# @pytest.mark.skip()
 def test_adds_movie_to_user_watchlist():
     # Arrange
     movie = {
@@ -120,7 +120,7 @@ def test_adds_movie_to_user_watchlist():
     assert updated_data["watchlist"][0]["genre"] == GENRE_1
     assert updated_data["watchlist"][0]["rating"] == RATING_1
 
-@pytest.mark.skip()
+# @pytest.mark.skip()
 def test_adds_movie_to_non_empty_user_watchlist():
     # Arrange
     movie = {
@@ -140,7 +140,7 @@ def test_adds_movie_to_non_empty_user_watchlist():
     assert movie in updated_data["watchlist"]
     assert FANTASY_2 in updated_data["watchlist"]
 
-@pytest.mark.skip()
+# @pytest.mark.skip()
 def test_moves_movie_from_watchlist_to_empty_watched():
     # Arrange
     janes_data = {
@@ -159,12 +159,17 @@ def test_moves_movie_from_watchlist_to_empty_watched():
     assert len(updated_data["watchlist"]) == 0
     assert len(updated_data["watched"]) == 1
     
-    raise Exception("Test needs to be completed.")
-    # *******************************************************************************************
-    # ****** Add assertions here to test that the correct movie was added to "watched" **********
-    # *******************************************************************************************
+    # raise Exception("Test needs to be completed.")
+    # # *******************************************************************************************
+    # # ****** Add assertions here to test that the correct movie was added to "watched" **********
+    # # *******************************************************************************************
+    watched_movie = updated_data["watched"][0]
 
-@pytest.mark.skip()
+    assert watched_movie["title"] == MOVIE_TITLE_1
+    assert watched_movie["genre"] == GENRE_1
+    assert watched_movie["rating"] == RATING_1
+
+# @pytest.mark.skip()
 def test_moves_movie_from_watchlist_to_watched():
     # Arrange
     movie_to_watch = HORROR_1
@@ -183,12 +188,15 @@ def test_moves_movie_from_watchlist_to_watched():
     assert len(updated_data["watchlist"]) == 1
     assert len(updated_data["watched"]) == 2
     
-    raise Exception("Test needs to be completed.")
-    # *******************************************************************************************
-    # ****** Add assertions here to test that the correct movie was added to "watched" **********
-    # *******************************************************************************************
+    # raise Exception("Test needs to be completed.")
+    # # *******************************************************************************************
+    # # ****** Add assertions here to test that the correct movie was added to "watched" **********
+    # # *******************************************************************************************
+    assert updated_data["watchlist"][0] == FANTASY_1
+    assert HORROR_1 in updated_data["watched"]
+    assert FANTASY_2 in updated_data["watched"]
 
-@pytest.mark.skip()
+# @pytest.mark.skip()
 def test_does_nothing_if_movie_not_in_watchlist():
     # Arrange
     movie_to_watch = HORROR_1
--- viewing_party/party.py
+++ viewing_party/party.py
@@ -9,9 +9,58 @@ def create_movie(title, genre, rating):
         }
     return None
 
+def add_to_watched(user_data, movie):
+    user_data["watched"].append(movie)
+    return user_data
+
+def add_to_watchlist(user_data, movie):
+    user_data["watchlist"].append(movie)
+    return user_data
+
+def watch_movie(user_data, title):
+    # look for a movie dict in watchlist with matching title 
+    for movie in user_data["watchlist"]:
+        if movie["title"] == title:   
+            user_data["watchlist"].remove(movie)
+            user_data["watched"].append(movie)
+            
+    return user_data     
+
+
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
+
+def get_watched_avg_rating(user_data):
+    if not user_data["watched"]:
+        return 0.0
+    total_rating = 0
+    for watched_movie in user_data["watched"]:
+       total_rating += watched_movie["rating"]
+    average_rating = total_rating/len(user_data["watched"])
+    return average_rating
+
+def get_most_watched_genre(user_data):
+    if not user_data["watched"]:
+        return None
+    genre_count = {}
+    for watched_movie in user_data["watched"]:
+       genre = watched_movie["genre"]
+       if genre in genre_count:
+           genre_count[genre] += 1
+       else:
+           genre_count[genre] = 1   
+
+    # find genre with hishest count
+    most_watched = None
+    highest_count = 0
+    for genre, count in genre_count.items():
+        if count > highest_count:
+            most_watched = genre
+            highest_count = count
+    return  most_watched      
+
+        
 
 
 # -----------------------------------------
